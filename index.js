13504477629677ad31edc0c3dc9e801446f79cf5
Nathan <nathjvella@gmail.com>
2026-08-01 00:39:36 +1000

    [Portfolio] - Added past project images and updated bio descriptions

--- index.js
+++ index.js
@@ -101,11 +101,18 @@ const skillLists = {
         "C#",
         "Unity Engine",
         "Maya",
+    ],
+
+    freeformSkillsList: [
+        "C#",
+        "Unity Engine",
     ]
 };
 
 for (const [containerId, skills] of Object.entries(skillLists)) {
     const container = document.getElementById(containerId);
+
+    if (container == null) continue;
 
     skills.forEach(skill => {
         const tag = document.createElement("span");
@@ -166,3 +173,38 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    let captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+} 
